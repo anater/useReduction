a12d7adabb27e5a0b0c9d7484dd75ea54269abda
Andrew Nater <a.a.nater@gmail.com>
2019-11-03 10:13:30 -0500

    Change returned value from `useReduction`
    Use an array instead of an object
    Update declaration to accomodate change
    Add comments and white space to declarations to improve readability

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,39 +1,44 @@
 import React from "react";
 
-interface Action<T = string> {
+// Actions
+
+type Action<T = string> = {
   type: T;
   payload: any;
-}
+};
+
+type ActionCreators<A> = {
+  [action: keyof A]: (payload: any) => void;
+};
+
+// Reducers
 
 type Reducer<S, A> = React.Reducer<S, Action<keyof A>>;
 
-interface ActionCreators<A> {
-  [action: keyof A]: (payload: any) => void;
-}
-
-interface ReducerMap<S, A> {
+type ReducerMap<S, A> = {
   [reducer: keyof A]: Reducer<S, A>;
-}
+};
 
-interface Reduction<S, A> {
-  state: S;
-  actions: ActionCreators<A>;
-}
+type Reduction<State, Action> = [State, ActionCreators<Action>];
 
 /**
  * Maps keys from `reducer` into `actions` which update `state`
  */
-export default function useReduction<S, A>(
-  initialState: S,
-  reducer: ReducerMap<S, A>,
+export default function useReduction<State, Action>(
+  initialState: State,
+  reducer: ReducerMap<State, Action>,
   debug: boolean
-): Reduction<S, A>;
+): Reduction<State, Action>;
 
-/** 
- * Builds `React.Reducer` from `reducerMap` 
+/**
+ * Builds `React.Reducer` from `reducerMap`
  */
 function makeReducer<S, A>(reducerMap: ReducerMap<S, A>): Reducer<S, A>;
 
+/**
+ * Builds action creators object from `reducerMap` keys.
+ * Action creators are bound to `dispatch` and will log actions when `debug` is true
+ */
 function makeActions<S, A>(
   reducerMap: ReducerMap<S, A>,
   dispatch: React.Dispatch<Action<keyof A>>,
