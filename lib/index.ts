28d32702d5e44a0939b4332eb20d92d085a6163b
Andrew Nater <a.a.nater@gmail.com>
2019-11-09 14:10:29 -0500

    Replace d.ts with typescript file

---
+++ lib/index.ts
@@ -0,0 +1,59 @@
+import { useReducer, Reducer, Dispatch } from "react";
+
+type Action<T, P> = {
+  type: T;
+  payload?: P;
+};
+
+type ActionMap<A> = {
+  [K in keyof A]: (payload?: any) => void;
+};
+
+type ReducerMap<S, A> = {
+  [K in keyof A]: Reducer<S, Action<K, any>>;
+};
+
+export default function useReduction<S, A>(
+  initialState: S,
+  reducerMap: ReducerMap<S, A>,
+  debug = false
+): [S, ActionMap<A>] {
+  const [state, dispatch] = useReducer(makeReducer(reducerMap), initialState);
+  return [state, makeActions(reducerMap, dispatch, debug)];
+}
+
+function makeReducer<S, A>(reducerMap: ReducerMap<S, A>) {
+  return (state: S, action: Action<keyof A, any>) => {
+    // if the dispatched action is valid and there's a matching reducer, use it
+    if (action && action.type && reducerMap[action.type]) {
+      return reducerMap[action.type](state, action);
+    } else {
+      // always return state if the action has no reducer
+      return state;
+    }
+  };
+}
+
+function makeActions<S, A>(
+  reducerMap: ReducerMap<S, A>,
+  dispatch: Dispatch<Action<keyof A, any>>,
+  debug: boolean
+): ActionMap<A> {
+  const types = Object.keys(reducerMap) as Array<keyof A>;
+  return types.reduce(
+    (actions: ActionMap<A>, type: keyof A) => {
+      // if there isn't already an action with this type
+      if (!actions[type]) {
+        // dispatches action with type and payload when called
+        actions[type] = (payload: any) => {
+          const action = { type, payload };
+          dispatch(action);
+          // optionally log actions
+          if (debug) console.log(action);
+        };
+      }
+      return actions;
+    },
+    {} as ActionMap<A>
+  );
+}
