ea03f7eb8dc79b764a91719b75c7c91d6c6facd1
Andrew Nater <a.a.nater@gmail.com>
2019-11-17 10:38:55 -0500

    Migrate build into TypeScript completely – dropping support for UMD/browsers generally since the idea here is to add into an existing project made via CRA or some other React starter. Assuming npm is the main entry and a bundler can manage import/export

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -2,7 +2,7 @@ import useReduction from "./index";
 
 const [state, actions] = useReduction(0, {
   reset(state, action) { 
-    return "bad"
+    return 0
   },
   increment: (state, {payload}) => state + payload
 });
